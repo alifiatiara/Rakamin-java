cae725d6f92872eac16d8e6d225fd0d8a71a1119
Alifia Tiara Hartono Putri <alifiatiarahp@gmail.com>
2023-10-16 11:34:31 +0700

    Create Java basic example

---
+++ src/main/java/firstProgram/firstProgram.java
@@ -0,0 +1,12 @@
+package firstProgram;
+
+public class firstProgram {
+    public static void main(String[] args) {
+        //Ini adalah program untuk print Hello, World!
+        System.out.println("Hello, World!");
+        //Print hello kedua
+        System.out.println("Hello, World!");
+        //Print hello ketiga
+        //System.out.println("Hello, World!");
+    }
+}
---
+++ src/main/java/firstProgram/percabangan.java
@@ -0,0 +1,27 @@
+package firstProgram;
+
+import java.util.Scanner;
+
+public class percabangan {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String password = prompt("Enter password :");
+        String validPassword = "password";
+
+        //Jika benar maka selamat datang bos
+        //Jika salah (else) maka error message
+        if(password.equals(validPassword)){
+            System.out.println("Selamat datang bos!");
+        }else{
+            System.out.println("Password salah,coba lagi");
+        }
+        System.out.println("Terima kasih sudah menggunakan aplikasi kami");
+    }
+
+    //Fungsi prompt untuk membaca input dari pengguna
+    public static String prompt(String message) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(message);
+        return scanner.nextLine();
+    }
+}
